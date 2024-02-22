d9190d9562474bee2fc2c15d94c05b7867ed1d70
Haddouche Maamar <frocodeof@gmail.com>
2024-02-22 20:34:30 +0100

    Change Column Values To Blooelan

--- Query.sql
+++ Query.sql
@@ -28,3 +28,20 @@ UPDATE dummy SET isHomeOwner = FALSE WHERE isHomeOwner = 0;
 UPDATE dummy SET Owns_Car = TRUE WHERE Owns_Car = 1;
 UPDATE dummy SET Owns_Car = FALSE WHERE Owns_Car = 0;
 DESCRIBE dummy;
+
+ALTER TABLE dummy
+MODIFY COLUMN  Owns_Car VARCHAR(5);
+
+UPDATE dummy
+SET indebt = 'TRUE'
+WHERE indebt = '1';
+
+UPDATE dummy
+SET isHomeOwner = 'TRUE'
+WHERE isHomeOwner = '1';
+
+UPDATE dummy
+SET Owns_Car = 'FALSE'
+WHERE Owns_Car = '0';
+
+SELECT * FROM dummy;
