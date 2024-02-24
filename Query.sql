16407d9bb0236d87f76332817826ce59e94a09c9
Haddouche Maamar <frocodeof@gmail.com>
2024-02-24 14:49:41 +0100

    AVG TimeSpent

--- Query.sql
+++ Query.sql
@@ -54,4 +54,22 @@ WHERE gender = 'non-binary';
 SELECT Gender, platform, COUNT(*) AS count
 FROM dummy
 GROUP BY Gender, platform
-ORDER BY Gender, COUNT(*) DESC;
+ORDER BY Gender , COUNT(*) DESC ;
+
+SELECT AVG(age) AS AvgAge
+FROM dummy;
+
+SELECT
+    CASE
+        WHEN age BETWEEN 0 AND 17 THEN '0-17'
+        WHEN age BETWEEN 18 AND 24 THEN '18-24'
+        WHEN age BETWEEN 25 AND 34 THEN '25-34'
+        WHEN age BETWEEN 35 AND 44 THEN '35-44'
+        WHEN age BETWEEN 45 AND 54 THEN '45-54'
+        WHEN age BETWEEN 55 AND 64 THEN '55-64'
+        ELSE '65+'
+    END AS AgeGroup,
+    AVG(TimeSpent) AS AverageTimeSpent
+FROM dummy
+GROUP BY AgeGroup
+ORDER BY AgeGroup;
